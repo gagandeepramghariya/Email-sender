6211dfece938f4b858986ec9ab6d17e42121fdfd
gagandeepramghariya <gagandeepvirdi2005@email.com>
2026-03-23 18:44:37 +0530

    some changes

--- newemail.py
+++ newemail.py
@@ -4,7 +4,7 @@ from finder import all_data
 ob=s.SMTP('smtp.gmail.com',587)
 ob.ehlo()
 ob.starttls()
-ob.login('gagandeep.official018@gmail.com','ujueanjgyjfhruud') 
+ob.login('gagandeep.official018@gmail.com','------------') 
 subject="test python"
 body="I Love Python"
 massage="subject:{}\n\n{}".format(subject,body)
