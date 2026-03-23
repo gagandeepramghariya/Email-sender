4043563125d57c47fd616323bc80f91500b0c66e
gagandeepramghariya <gagandeepvirdi2005@email.com>
2026-03-23 12:11:33 +0530

    crate a email sender code in python

--- newemail.py
+++ newemail.py
@@ -1,1 +1,14 @@
-import 
+import  smtplib as s 
+
+ob=s.SMTP('smtp.gmail.com',587)
+ob.ehlo()
+ob.starttls()
+ob.login('gagandeep.official018@gmail.com','ujueanjgyjfhruud') 
+subject="test python"
+body="I Love Python"
+massage="subject:{}\n\n{}".format(subject,body)
+listadd=['gagandeepvirdi2005@gmail.com']
+
+ob.sendmail('gagandeep.officil@gmail.com ',listadd,massage) 
+print("send mail ")
+ob.quit()
